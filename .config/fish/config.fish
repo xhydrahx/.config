4a7d81f59b6f2c21985c08963a9e3ebccd1637ba
a-creamy <akramo@tuta.io>
2024-12-14 13:27:22 +0100

    Added padding again

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
   set -U fish_greeting ""
   set -U fish_user_paths /opt/novnc/utils $fish_user_paths
+  set hydro_symbol_git_ahead (printf '\u2191 ')
 end
 
 set -x CC clang
