3dc4948ee0798185219bb5a8158404dfae83f647
a-creamy <akramo@tuta.io>
2024-12-15 21:21:52 +0100

    Changed to Fira Code

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
   set -U fish_greeting ""
   set -U fish_user_paths /opt/novnc/utils $fish_user_paths
-  set hydro_symbol_git_ahead (printf '\u2191 ')
 
   alias scrot="scrot ~/media/scrot/%Y-%m-%d_%H-%M-%S.png"
 end
