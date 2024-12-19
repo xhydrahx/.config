4b5b03ce40da6dee74c6065af076bd09ba2b808e
a-creamy <akramo@tuta.io>
2024-12-19 23:01:38 +0100

    Too poor for ai

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
   set -U fish_greeting ""
   set -U fish_user_paths /opt/novnc/utils $fish_user_paths
-  set -x ANTHROPIC_API_KEY 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
 
   alias scrot="scrot ~/media/scrot/%Y-%m-%d_%H-%M-%S.png"
 end
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -11,42 +11,6 @@ vim.opt.termguicolors = true
 
 require("lazy").setup({
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false,
-    opts = {},
-    build = "make",
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "hrsh7th/nvim-cmp",
-      "nvim-tree/nvim-web-devicons",
-      {
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
-  {
     "uga-rosa/ccc.nvim",
     config = function()
       require("ccc").setup({
