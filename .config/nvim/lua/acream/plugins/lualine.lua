472bc03c7120fdc3c5be8b2632f1f446617c2653
a-creamy <akramo@tuta.io>
2025-02-08 21:22:04 +0000

    Format codr

--- .config/nvim/lua/acream/plugins/lualine.lua
+++ .config/nvim/lua/acream/plugins/lualine.lua
@@ -2,81 +2,81 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require('lualine').setup({
-  options = {
-    globalstatus = true,
-    theme = "auto",
-    icons_enabled = true,
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = {
-      {
-        'mode',
-        fmt = function(str)
-          local mode_map = {
-            ['NORMAL'] = ' NORMAL',
-            ['INSERT'] = ' INSERT',
-            ['VISUAL'] = '󰒅 VISUAL',
-            ['V-LINE'] = '󰕷 V-LINE',
-            ['V-BLOCK'] = '󰝤 V-BLOCK',
-            ['REPLACE'] = '󰑌 REPLACE',
-            ['COMMAND'] = ' COMMAND',
-            ['TERMINAL'] = ' TERMINAL',
-          }
-          return mode_map[str] or str
-        end,
-      }
-    },
-    lualine_b = {
-      {
-        'filename',
-        path = 0,
-        symbols = {
-          modified = '●',
-          readonly = '●',
-          unnamed = '[No Name]',
-        },
-        fmt = function(str)
-          local icon = require('nvim-web-devicons').get_icon(vim.fn.expand('%:t'), vim.fn.expand('%:e'),
-            { default = true })
-          return icon .. ' ' .. str
-        end,
-      },
-      {
-        'diagnostics',
-        sources = { 'nvim_lsp' },
-        sections = { 'error', 'warn', 'info', 'hint' },
-        diagnostics_color = {
-          error = { fg = '#dc322f' },
-          warn = { fg = '#b58900' },
-          info = { fg = '#268bd2' },
-          hint = { fg = '#2aa198' },
-        },
-      },
-    },
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = { "branch" },
-    lualine_z = {
-      {
-        'progress',
-        fmt = function(str)
-          return ' ' .. str
-        end,
-      }
-    },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {},
-  },
-  extensions = {},
-})
-end,
+        require('lualine').setup({
+            options = {
+                globalstatus = true,
+                theme = "auto",
+                icons_enabled = true,
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+            },
+            sections = {
+                lualine_a = {
+                    {
+                        'mode',
+                        fmt = function(str)
+                            local mode_map = {
+                                ['NORMAL'] = ' NORMAL',
+                                ['INSERT'] = ' INSERT',
+                                ['VISUAL'] = '󰒅 VISUAL',
+                                ['V-LINE'] = '󰕷 V-LINE',
+                                ['V-BLOCK'] = '󰝤 V-BLOCK',
+                                ['REPLACE'] = '󰑌 REPLACE',
+                                ['COMMAND'] = ' COMMAND',
+                                ['TERMINAL'] = ' TERMINAL',
+                            }
+                            return mode_map[str] or str
+                        end,
+                    }
+                },
+                lualine_b = {
+                    {
+                        'filename',
+                        path = 0,
+                        symbols = {
+                            modified = '●',
+                            readonly = '●',
+                            unnamed = '[No Name]',
+                        },
+                        fmt = function(str)
+                            local icon = require('nvim-web-devicons').get_icon(vim.fn.expand('%:t'), vim.fn.expand('%:e'),
+                                { default = true })
+                            return icon .. ' ' .. str
+                        end,
+                    },
+                    {
+                        'diagnostics',
+                        sources = { 'nvim_lsp' },
+                        sections = { 'error', 'warn', 'info', 'hint' },
+                        diagnostics_color = {
+                            error = { fg = '#dc322f' },
+                            warn = { fg = '#b58900' },
+                            info = { fg = '#268bd2' },
+                            hint = { fg = '#2aa198' },
+                        },
+                    },
+                },
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = { "branch" },
+                lualine_z = {
+                    {
+                        'progress',
+                        fmt = function(str)
+                            return ' ' .. str
+                        end,
+                    }
+                },
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+            extensions = {},
+        })
+    end,
 }
