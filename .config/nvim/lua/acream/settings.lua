fc3a645b7854e6fcd02929af21b2dd4ec0c25549
a-creamy <akramo@tuta.io>
2025-02-18 20:06:34 +0000

    Changed up a few things

--- .config/nvim/lua/acream/settings.lua
+++ .config/nvim/lua/acream/settings.lua
@@ -6,7 +6,16 @@ vim.opt.smartindent = true
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.statuscolumn = "%=%{v:relnum > 0 ? v:relnum : v:lnum} "
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+        if vim.bo.buftype == "" then
+            vim.opt.statuscolumn = "%=%{v:relnum > 0 ? v:relnum : v:lnum} "
+        else
+            vim.opt.statuscolumn = ""
+        end
+    end
+})
 
 vim.g.mapleader = " "
 
@@ -14,14 +23,20 @@ vim.keymap.set("n", "<leader>ff", function()
     Snacks.picker.files()
 end)
 
-vim.keymap.set("n", "<leader>fg", function ()
+vim.keymap.set("n", "<leader>fg", function()
     Snacks.picker.grep()
 end)
 
-vim.keymap.set("n", "<leader>fr", function ()
+vim.keymap.set("n", "<leader>fr", function()
     Snacks.picker.recent()
 end)
 
-vim.keymap.set("n", "<leader>e", function ()
+vim.keymap.set("n", "<leader>e", function()
     Snacks.explorer()
 end)
+
+vim.keymap.set("v", "<leader>aa", "<cmd>AvanteAsk<CR>")
+vim.keymap.set("v", "<leader>ae", "<cmd>AvanteEdit<CR>")
+
+vim.keymap.set("n", "<leader>aa", "<cmd>AvanteAsk<CR>")
+vim.keymap.set("n", "<leader>ac", "<cmd>AvanteClear<CR>")
