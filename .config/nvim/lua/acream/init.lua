af197dc54cc367af5df33e6d4c26dd6d59fe8fc7
a-creamy <akramo@tuta.io>
2025-04-16 17:57:20 +0200

    Def something

--- .config/nvim/lua/acream/init.lua
+++
@@ -1,3 +0,0 @@
-require("acream.settings")
-require("acream.lazy")
-
--- .config/nvim/lua/acream/plugins/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "sainnhe/gruvbox-material",
-    config = function()
-        vim.g.gruvbox_material_background = "hard"
-        vim.g.gruvbox_material_palette = "material"
-        vim.cmd("colorscheme gruvbox-material")
-    end
-}
