3a6ab023c4cc6dd22fe72f57f6026e9b3d1db959
a-creamy <akramo@tuta.io>
2025-04-18 22:02:56 +0200

    On top

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if [ -d "$HOME/.config" ]; then
-  rsync -av --delete .config/ "$HOME/.config/"
-else
-  mkdir -p "$HOME/.config"
-  rsync -av --delete .config/ "$HOME/.config/"
-fi
+cp -r .config ~/.config || echo "Could not copy .config dir" ; exit 1
+
+ln -s ~/.config .config
+ln -s ~/.config/vim/vimrc .vimrc
