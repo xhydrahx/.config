bf38e0af578e14991a52f11c3b3f047b5cfe0f9f
a-creamy <akramo@tuta.io>
2025-04-23 22:43:29 +0200

    K

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 cp -r .config ~/.config || echo "Could not copy .config dir" ; exit 1
 
 ln -s ~/.config .config
+rm -rf .vimrc
+touch .vimrc
 ln -s ~/.config/vim/vimrc .vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
