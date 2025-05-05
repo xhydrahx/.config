50df13ad882b633f6bd457f87e0b6606f32b0ab4
a-creamy <akramo@tuta.io>
2025-05-05 14:39:07 +0000

    Cleanup

--- .config/emacs/elpa/fzf-20240822.201/fzf-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "fzf" "20240822.201"
-  "A front-end for fzf."
-  '((emacs "24.4"))
-  :url "https://github.com/bling/fzf.el"
-  :commit "641aef33c88df3733f13d559bcb2acc548a4a0c3"
-  :revdesc "641aef33c88d"
-  :keywords '("fzf" "fuzzy" "search"))
