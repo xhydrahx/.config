a3a84a57d4450642efdcb0475e824be016c13ce1
a-creamy <akramo@tuta.io>
2025-05-18 16:51:51 +0000

    Actuall working setup :)

--- .config/nvim/lua/acream/plugins/ui/lualine.lua
+++ .config/nvim/lua/acream/plugins/ui/lualine.lua
@@ -12,60 +12,17 @@ return {
             },
             sections = {
                 lualine_a = {
-                    {
-                        'mode',
-                        fmt = function(str)
-                            local mode_map = {
-                                ['NORMAL'] = ' NORMAL',
-                                ['INSERT'] = ' INSERT',
-                                ['VISUAL'] = '󰒅 VISUAL',
-                                ['V-LINE'] = '󰕷 V-LINE',
-                                ['V-BLOCK'] = '󰝤 V-BLOCK',
-                                ['REPLACE'] = '󰑌 REPLACE',
-                                ['COMMAND'] = ' COMMAND',
-                                ['TERMINAL'] = ' TERMINAL',
-                            }
-                            return mode_map[str] or str
-                        end,
-                    }
+                    'mode',
                 },
                 lualine_b = {
-                    {
-                        'filename',
-                        path = 0,
-                        symbols = {
-                            modified = '●',
-                            readonly = '●',
-                            unnamed = '[No Name]',
-                        },
-                        fmt = function(str)
-                            local icon = require('nvim-web-devicons').get_icon(vim.fn.expand('%:t'), vim.fn.expand('%:e'),
-                                { default = true })
-                            return icon .. ' ' .. str
-                        end,
-                    },
-                    {
-                        'diagnostics',
-                        sources = { 'nvim_lsp' },
-                        sections = { 'error', 'warn', 'info', 'hint' },
-                        diagnostics_color = {
-                            error = { fg = '#dc322f' },
-                            warn = { fg = '#b58900' },
-                            info = { fg = '#268bd2' },
-                            hint = { fg = '#2aa198' },
-                        },
-                    },
+                    'filename',
+                    'diagnostics',
                 },
                 lualine_c = {},
                 lualine_x = {},
                 lualine_y = { "branch" },
                 lualine_z = {
-                    {
-                        'progress',
-                        fmt = function(str)
-                            return ' ' .. str
-                        end,
-                    }
+                    'progress',
                 },
             },
             inactive_sections = {
