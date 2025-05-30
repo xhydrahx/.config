ba4e8b04120da4ccdbd4cc15be78cc7cf4c43525
a-creamy <akramo@tuta.io>
2025-05-30 09:05:43 +0000

    Added rust to path

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# pnpm
+set -gx PNPM_HOME "/home/acream/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+set -gx PATH /usr/src/zig-aarch64-linux-0.15.0-dev.643+dc6ffc28b $PATH
+
+source "$HOME/.cargo/env.fish"
